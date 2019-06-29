368286d27482d691525dfb81764b330d817bcd76
akash121111 <akashkumar121111@gmail.com>
2019-06-29 21:39:05 +0530

    Add Association between user and Article And also debuuger to the browser

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    has_many :comments 
+    has_many :articles
     has_secure_password
+
+    before_save { self.email = email.downcase }
 
 validates :username, presence: true,
 
